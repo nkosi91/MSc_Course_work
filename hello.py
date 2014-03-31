460693be9562527f04b3c27dbc03782b16b4afeb
Nkosi91 <joy.hlatshwayo@gmail.com>
2014-03-31 16:09:02 +0200

    Changed hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print "hello git people who spend time thinking about how to complicate life in order to simplify it"
+print "hello git people who spend time thinking about how to complicate life in order to simplify it, how tiring?"
